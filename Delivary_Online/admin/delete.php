aaf8eacbbbccee6b8a5bfff9d5bf8aaaed613cbe
amerabenamer <38440364+AmeraBenAmer@users.noreply.github.com>
2019-01-30 15:37:57 +0200

    update admin site

--- Delivary_Online/admin/delete.php
+++
@@ -1,7 +0,0 @@
-<?php
-    	$connect = mysqli_connect("localhost","root","","deliveryrest");
-        $sql = "DELETE FROM USERS WHERE id = '".$_POST["id"]."'";
-        if(mysqli_query($connect,$sql)){
-            echo 'Data Deleted';
-        }
-?>
--- Delivary_Online/admin/edit.php
+++
@@ -1,9 +0,0 @@
-<?php
-    
-    $connect = mysqli_connect("localhost","root","","deliveryrest");
-    $id = $_POST["id"];
-    $text = $_POST["text"];
-    $column_name = $_POST["column_name"];
-     
-
-?>
